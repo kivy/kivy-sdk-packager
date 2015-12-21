0f9dddb1986952361149a4e4bfbce0693f319ceb
akshayaurora <akshayaurora@gmail.com>
2015-12-22 01:31:39 +0530

    Support for Python3 embedding and cleanup, support for specifying bundle arguments.

---
+++ osx/cleanup_app.sh
@@ -0,0 +1,28 @@
+# Cleanup script for cleaning up <yourapp>.app
+#
+#
+
+if [ 'x$1' == 'x' ]; then
+    echo "Usage: cleanup_app.py <app_name.app>"
+    exit 1
+fi
+
+APPPATH=$1
+
+PYPATH="$APPPATH/Contents/Frameworks/python"
+rm -rf "$APPPATH/"Contents/Resources/kivy/{doc,build,examples}
+rm -rf "$APPPATH/"Contents/Resources/kivy/kivy/{tools,tests}
+rm -rf "$APPPATH/"Contents/Resources/venv/bin/{cython*,cygdb,osxrelocator,pip*,pygmentize,easy_install*,rst*}
+rm -rf "$APPPATH/"Contents/Resources/venv/lib/python2.7/site-packages/{pip*,Cython*,setuptools*,osxrelocator*,cython*}
+rm -rf "$APPPATH/"Contents/Resources/venv/lib/python2.7/site-packages/wheel/test
+rm -rf "$APPPATH/"Contents/Resources/venv/lib/python3.5/site-packages/{pip*,Cython*,setuptools*,osxrelocator*,cython*,wheel/test*}
+rm -rf "$APPPATH/"Contents/Resources/venv/lib/python3.5/site-packages/wheel/test
+rm -rf "$PYPATH/"3.5.0/lib/python3.5/{turtledemo,test,unittest,curses,distutils,ensurepip,idlelib,pydoc_data,setuptools*}
+rm -rf "$PYPATH/"3.5.0/lib/python3.5/site-packages/{easy_install*,pip*}
+rm -rf "$PYPATH/"3.5.0/lib/python3.5/site-packages/wheel/test
+rm -rf "$PYPATH/3.5.0/lib/python3.5/sqlite3"
+rm -rf "$PYPATH/3.5.0/lib/python3.5/tkinter"
+rm -rf "$PYPATH/"3.5.0/bin/{pygmentize,2to*,pip*,*-config,easy_install*,idle*,pydoc*,python3.5m*,rst*,pip*}
+rm -rf "$PYPATH/"3.5.0/lib/{lib*,pkgconfig}
+rm -rf "$PYPATH/"3.5.0/include
+rm -rf "$PYPATH/3.5.0/lib/pkgconfig"

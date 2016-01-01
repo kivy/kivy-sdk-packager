b9ff647b827a11f9dc5307494447b6fe8634ff0f
Thomas-Karl Pietrowski <thopiekar@googlemail.com>
2016-01-01 15:58:35 +0100

    Restoring content to b1fc346e66bbc18b826e742a3a2e761d43e7f530

--- osx/README.md
+++ osx/README.md
@@ -3,9 +3,25 @@ Kivy for OS X
 
 This repository contains the scripts for:
 
-- creating a Kivy.app directory from scratch
+- creating a Kivy.app directory from scratch 
+
+    - `./create-osx-bundle`: will create a Kivy.app using system python and stable branch of kivy.
+    - `./create-osx-bundle python2 master`: will create a Kivy.app using system python and master branch of kivy.
+    - `./create-osx-bundle python3`: will create a Kivy.app embedding python3 and stable branch of kivy.
+    - `./create-osx-bundle python3 master`: will create a Kivy.app embedding python3 and master branch of kivy.
+    
 - packaging a Kivy application using the previously generated Kivy.app
+
+    `./package_app.py --help` to get more info
+
 - creating a package (.dmg) of an application (.app)
+
+    `./create-osx-dmg.sh appname.app`
+
+To **install custom packages** into your app just pass them as `--deps=dep1,dep2,dep3` to `package_app.py`
+Deps are installed using pip so you can pass arguments like --deps=cython==0.23
+
+To include binaries into the package just put them under app_name.app/Contents/Resources/venv/bin folder. 
 
 This approach works starting from Kivy 1.9, with SDL2 and GStreamer in mind.
 Anything else is untested.
---
+++ win/sdl2.py
@@ -0,0 +1,91 @@
+from __future__ import absolute_import, print_function
+import sys
+from os.path import join, sep
+from os import walk
+from .common import *
+
+__version__ = '0.1.2'
+
+sdl2_ver = '2.0.3'
+sdl2_mixer_ver = '2.0.0'
+sdl2_ttf_ver = '2.0.12'
+sdl2_image_ver = '2.0.0'
+
+drive_map = {
+    'SDL2-devel-{}-mingw.tar.gz'.format(sdl2_ver): '0B1_HB9J8mZepUXlqeDFPd0oybk0',
+    'SDL2_mixer-devel-{}-mingw.tar.gz'.format(sdl2_mixer_ver): '0B1_HB9J8mZepQ1l4WlUyT0t2ODQ',
+    'SDL2_ttf-devel-{}-mingw.tar.gz'.format(sdl2_ttf_ver): '0B1_HB9J8mZepdjRSdVVZdjBjWFE',
+    'SDL2_image-devel-{}-mingw.tar.gz'.format(sdl2_image_ver): '0B1_HB9J8mZepOXNoSWJCYlI3QW8',
+    'SDL_platform.h': '0B1_HB9J8mZepNmZjcVFtRklINzA'
+}
+
+
+def get_gdrive_link(fname):
+    gid = drive_map[fname]
+    url = 'https://drive.google.com/uc?export=download&id={}'.format(gid)
+    return url
+
+
+def get_sdl2(cache, build_path, arch, pyver, package, output):
+    data = []
+
+    for name, ver in (
+        ('https://www.libsdl.org/release/SDL2-devel-{}-mingw.tar.gz',
+         sdl2_ver),
+        ('https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-{}-mingw.tar.gz',
+         sdl2_mixer_ver),
+        ('http://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-{}-mingw.tar.gz',
+         sdl2_ttf_ver),
+        ('http://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-{}-mingw.tar.gz',
+         sdl2_image_ver)):
+        url = name.format(ver)
+        fname = url.split('/')[-1]
+        url = get_gdrive_link(fname)
+        local_url = download_cache(cache, url, build_path, fname)
+
+        exec_binary(
+            'Extracting {}'.format(local_url),
+            [zip7, 'x', '-y', local_url], cwd=build_path, shell=True, exclude=zip_q)
+        exec_binary(
+            'Extracting {}'.format(local_url[:-3]),
+            [zip7, 'x', '-y', local_url[:-3]], cwd=build_path, shell=True, exclude=zip_q)
+
+        base_dir = local_url.replace('-mingw.tar.gz', '').replace('-devel', '')
+        if arch == '64':
+            base_dir = join(base_dir, 'x86_64-w64-mingw32')
+        else:
+            base_dir = join(base_dir, 'i686-w64-mingw32')
+
+        if fname.startswith('SDL2-'):
+            url = 'https://hg.libsdl.org/SDL/raw-file/e217ed463f25/include/SDL_platform.h'
+            url = get_gdrive_link('SDL_platform.h')
+            local_url = download_cache(cache, url, join(base_dir, 'include', 'SDL2'),
+                                       'SDL_platform.h')
+
+        for d in ('lib', 'include', 'bin'):
+            # bin goes to python/share/kivy_package
+            src = join(base_dir, d)
+            for dirpath, dirnames, filenames in walk(src):
+                root = dirpath
+
+                if d == 'bin':
+                    base = join('share', package, 'bin')
+                elif d == 'lib':
+                    base = 'libs'
+                else:
+                    base = d
+
+                dirpath = dirpath.replace(src, '')
+                if dirpath and dirpath[0] == sep:
+                    dirpath = dirpath[1:]
+
+                for filename in filenames:
+                    data.append((
+                        join(root, filename), join(d, dirpath, filename),
+                        join(base, dirpath), d != 'bin'))
+
+    make_package(join(build_path, 'project'), package, data, __version__, output, 'zlib')
+
+
+if __name__ == '__main__':
+    parse_args(get_sdl2)

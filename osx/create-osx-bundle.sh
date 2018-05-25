cef30745c812520d15fd4eedf77c572281d7ad34
Akshay Arora <akshayaurora@gmail.com>
2018-05-26 00:59:51 +0530

    Update create-osx-bundle.sh

--- osx/create-osx-bundle.sh
+++ osx/create-osx-bundle.sh
@@ -112,7 +112,7 @@ fi
 
 echo "-- Install dependencies"
 source venv/bin/activate
-pip install cython==0.25.2
+pip install cython --upgrade
 $PYTHON -m pip install pygments docutils
 pip install git+http://github.com/tito/osxrelocator
 echo "-- Link python to the right location for relocation"

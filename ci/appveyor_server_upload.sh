ed829b9af32bde65f6ecbb4cc6fbdf3597217b25
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 18:49:58 -0400

    Fix inclusion.

--- ci/appveyor_server_upload.sh
+++ ci/appveyor_server_upload.sh
@@ -5,4 +5,4 @@ fi
 echo -e "$1\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 cp "$(dirname "$0")/id_rsa" ~/.ssh/id_rsa
 echo "copying from $(cygpath -u "$2"), $3 to root@$1:/web/downloads/$4"
-rsync -avh -e --include=$3 "ssh -p 2458" "$(cygpath -u "$2")" root@$1:/web/downloads/$4
+rsync -avh -e "ssh -p 2458" --include="*/" --include="$3" --exclude="*" "$(cygpath -u "$2")" "root@$1:/web/downloads/$4"

5fec365b2fa718557cf605d48dc942aee72344f8
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 18:31:37 -0400

    Fix upload pat.

--- ci/appveyor_server_upload.sh
+++ ci/appveyor_server_upload.sh
@@ -4,4 +4,5 @@ if [ ! -d "/home/appveyor/.ssh" ]; then
 fi
 echo -e "$1\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 cp "$(dirname "$0")/id_rsa" ~/.ssh/id_rsa
-rsync -avh -e "ssh -p 2458" "$(cygpath -u "$2")/$3" root@$1:/web/downloads/$4
+echo "copying from $(cygpath -u "$2"), $3 to root@$1:/web/downloads/$4"
+rsync -avh -e --include=$3 "ssh -p 2458" "$(cygpath -u "$2")" root@$1:/web/downloads/$4

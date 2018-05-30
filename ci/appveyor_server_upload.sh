f1b7e542522af9a532d3d8e1b278274f9483268e
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 19:07:23 -0400

    Copy contents, not directory.

--- ci/appveyor_server_upload.sh
+++ ci/appveyor_server_upload.sh
@@ -5,4 +5,4 @@ fi
 echo -e "Host $1\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 cp "$(dirname "$0")/id_rsa" ~/.ssh/id_rsa
 echo "copying from $(cygpath -u "$2"), $3 to root@$1:/web/downloads/$4"
-rsync -avh -e "ssh -p 2458" --include="*/" --include="$3" --exclude="*" "$(cygpath -u "$2")" "root@$1:/web/downloads/$4"
+rsync -avh -e "ssh -p 2458" --include="*/" --include="$3" --exclude="*" "$(cygpath -u "$2")/" "root@$1:/web/downloads/$4"

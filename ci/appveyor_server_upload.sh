d54faa401f0cdcc653761c9484566dde163e3f85
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 18:57:46 -0400

    Missing host.

--- ci/appveyor_server_upload.sh
+++ ci/appveyor_server_upload.sh
@@ -2,7 +2,7 @@ pacman -S --noconfirm git rsync
 if [ ! -d "/home/appveyor/.ssh" ]; then
   mkdir "/home/appveyor/.ssh"
 fi
-echo -e "$1\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "Host $1\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 cp "$(dirname "$0")/id_rsa" ~/.ssh/id_rsa
 echo "copying from $(cygpath -u "$2"), $3 to root@$1:/web/downloads/$4"
 rsync -avh -e "ssh -p 2458" --include="*/" --include="$3" --exclude="*" "$(cygpath -u "$2")" "root@$1:/web/downloads/$4"

5c324ee08cb9411e2da00d696bbdedd319f8c032
Matthew Einhorn <moiein2000@gmail.com>
2019-12-24 22:11:25 -0500

    USe 2019 platform.

--- win/glew.py
+++ win/glew.py
@@ -10,7 +10,7 @@ glew_ver = '2.1.0'
 batch = '''
 call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\Tools\\VsDevCmd.bat"
 devenv /upgrade .\\build\\vc12\\glew.sln
-msbuild .\\build\\vc12\\glew.sln /property:Configuration=Release /property:Platform={} /p:PlatformToolset=v140
+msbuild .\\build\\vc12\\glew.sln /property:Configuration=Release /property:Platform={}
 '''
 
 

74cb5ec169677d0a2c2d2c1364ee5da94045d10e
Matthew Einhorn <moiein2000@gmail.com>
2019-12-28 06:10:24 -0500

    Clear python from path.

--- win/glew.py
+++ win/glew.py
@@ -8,6 +8,7 @@ glew_ver = '2.1.0'
 
 batch = '''
 call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\Tools\\VsDevCmd.bat"
+devenv /upgrade .\\build\\vc15\\glew.sln
 msbuild .\\build\\vc15\\glew.sln /property:Configuration=Release /property:Platform={}
 '''
 

80fef7f845c8b42fe6ac7c22ea815d745b708e0c
Mirko Galimberti <me@mirkogalimberti.com>
2024-06-08 17:46:56 +0200

    Added SDL3 wheels for kivy_deps.sdl3 and kivy_deps.sdl3_dev

---
+++ win/build_sdl3.ps1
@@ -0,0 +1,144 @@
+# Extract the build architecture from the arguments
+param(
+    [string]$build_arch = "x64"
+)
+
+# Check if KIVY_DEPS_BUILD is set
+if ($env:KIVY_DEPS_BUILD) {
+    Write-Host "KIVY_DEPS_BUILD is set. Powershell script should be run in a clean environment."
+    exit 1
+}
+# Set KIVY_DEPS_BUILD to 1
+$env:KIVY_DEPS_BUILD = 1
+
+Write-Host "Building SDL3 dependencies for $build_arch architecture."
+
+# Cleaning the kivy-dependencies/download, kivy-dependencies/build and kivy-dependencies/dist folders
+Write-Host "Cleaning the kivy-dependencies folder..."
+Remove-Item -Path "kivy-dependencies" -Force -Recurse -ErrorAction SilentlyContinue
+
+# Create the kivy-dependencies folder
+New-Item -ItemType Directory -Path kivy-dependencies
+
+# Create the kivy-dependencies/Powershell folder
+New-Item -ItemType Directory -Path kivy-dependencies/WindowsPowerShell/Modules
+
+pushd kivy-dependencies
+Write-Host "Downloading the Visual Studio Setup PowerShell module ..."
+Invoke-WebRequest -Uri "https://github.com/microsoft/vssetup.powershell/releases/download/2.2.12-65a71775b3/VSSetup.zip" -OutFile "VSSetup.zip"
+Expand-Archive VSSetup.zip "WindowsPowerShell\Modules\VSSetup"
+Write-Host "Visual Studio Setup PowerShell module downloaded."
+popd
+
+Write-Host "Adding the Visual Studio Setup PowerShell module to the PSModulePath ..."
+$env:PSModulePath = $env:PSModulePath + ";$(pwd)\kivy-dependencies\WindowsPowerShell\Modules"
+
+# Get the Visual Studio installation path
+$vs_install_path = Get-VSSetupInstance | Where-Object { $_.InstallationPath -ne $null } | Select-Object -ExpandProperty InstallationPath
+Write-Host "Visual Studio installation path: $vs_install_path"
+
+# Set the environment variables via vcvarsall.bat
+$vcvars_path = "$vs_install_path/VC/Auxiliary/Build/vcvarsall.bat"
+Write-Host "vcvarsall path: $vcvars_path"
+
+# Call the .bat file from the PowerShell script
+cmd.exe /c " `"$vcvars_path`" $build_arch && set" | ForEach-Object {
+    if ($_ -match "=") {
+        $v = $_.split("=", 2);
+        Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])";
+        Write-host "Setting $($v[0]) to $($v[1])"
+    }
+};
+
+write-host "`nVisual Studio Command Prompt variables set." -ForegroundColor Yellow
+
+
+# Create the kivy-dependencies/download folder
+New-Item -ItemType Directory -Path kivy-dependencies/download
+
+# Create the build folder for the dependencies
+New-Item -ItemType Directory -Path kivy-dependencies/build  
+
+# Create the dist folder for the dependencies
+New-Item -ItemType Directory -Path kivy-dependencies/dist
+
+# windows SDL3
+$WINDOWS__SDL3__VERSION = "3.1.2"
+$WINDOWS__SDL3__URL="https://github.com/libsdl-org/SDL/archive/refs/heads/main.tar.gz"
+$WINDOWS__SDL3__FOLDER="SDL-main"
+
+# windows SDL3_image
+$WINDOWS__SDL3_IMAGE__URL="https://github.com/libsdl-org/SDL_image/archive/refs/heads/main.tar.gz"
+$WINDOWS__SDL3_IMAGE__FOLDER="SDL_image-main"
+
+# windows SDL3_mixer
+$WINDOWS__SDL3_MIXER__URL="https://github.com/libsdl-org/SDL_mixer/archive/refs/heads/main.tar.gz"
+$WINDOWS__SDL3_MIXER__FOLDER="SDL_mixer-main"
+
+# windows SDL3_ttf
+$WINDOWS__SDL3_TTF__URL="https://github.com/libsdl-org/SDL_ttf/archive/refs/heads/main.tar.gz"
+$WINDOWS__SDL3_TTF__FOLDER="SDL_ttf-main"
+
+# Download the dependencies
+Write-Host "Downloading the dependencies..."
+Write-Host "-- SDL3, url: $WINDOWS__SDL3__URL"
+Invoke-WebRequest -Uri $WINDOWS__SDL3__URL -OutFile "kivy-dependencies/download/SDL3-$WINDOWS__SDL3__VERSION.tar.gz"
+Invoke-WebRequest -Uri $WINDOWS__SDL3_IMAGE__URL -OutFile "kivy-dependencies/download/SDL_image-main.tar.gz"
+Invoke-WebRequest -Uri $WINDOWS__SDL3_MIXER__URL -OutFile "kivy-dependencies/download/SDL_mixer-main.tar.gz"
+Invoke-WebRequest -Uri $WINDOWS__SDL3_TTF__URL -OutFile "kivy-dependencies/download/SDL_ttf-main.tar.gz"
+
+# Save dist folder full path
+$dist_folder = (Get-Item -Path ".\kivy-dependencies\dist").FullName
+
+pushd kivy-dependencies/build
+
+Write-Host "Extracting the dependencies..."
+# Extract the dependencies
+tar -xf "../download/SDL3-$WINDOWS__SDL3__VERSION.tar.gz"
+tar -xf "../download/SDL_image-main.tar.gz"
+tar -xf "../download/SDL_mixer-main.tar.gz"
+tar -xf "../download/SDL_ttf-main.tar.gz"
+
+# Move into the SDL3 folder
+Write-Host "-- Build SDL3"
+cd "SDL-main"
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="$dist_folder" -DCMAKE_BUILD_TYPE=Release -GNinja
+cmake --build build/ --config Release --verbose --parallel
+cmake --install build/ --config Release
+
+cd ..
+
+# Move into the SDL_mixer folder
+Write-Host "-- Build SDL_mixer"
+cd "SDL_mixer-main"
+./external/Get-GitModules.ps1
+cmake -B build -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DSDL3MIXER_VENDORED=ON  -DSDL3_DIR="$dist_folder/cmake"  -DCMAKE_INSTALL_PREFIX="$dist_folder" -GNinja
+cmake --build build/ --config Release --parallel --verbose
+cmake --install build/ --config Release
+cd ..
+
+# Move into the SDL_image folder
+Write-Host "-- Build SDL_image"
+cd "SDL_image-main"
+./external/Get-GitModules.ps1
+cmake -B build -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DSDL3IMAGE_TIF_VENDORED=ON -DSDL3IMAGE_WEBP_VENDORED=ON -DSDL3IMAGE_JPG_VENDORED=ON -DSDL3IMAGE_PNG_VENDORED=ON -DSDL3IMAGE_TIF_SHARED=OFF -DSDL3IMAGE_WEBP_SHARED=OFF  -DSDL3IMAGE_VENDORED=OFF -DSDL3_DIR="$dist_folder/cmake"  -DCMAKE_INSTALL_PREFIX="$dist_folder" -DCMAKE_POLICY_DEFAULT_CMP0141=NEW -GNinja
+cmake --build build/ --config Release --parallel --verbose
+cmake --install build/ --config Release
+cd ..
+
+# Move into the SDL_ttf folder
+Write-Host "-- Build SDL_ttf"
+cd "SDL_ttf-main"
+./external/Get-GitModules.ps1
+cmake -B build-cmake -DBUILD_SHARED_LIBS=ON -DSDL3TTF_HARFBUZZ=ON -DFT_DISABLE_PNG=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DSDL3TTF_VENDORED=ON -DSDL3_DIR="$dist_folder/cmake"  -DCMAKE_INSTALL_PREFIX="$dist_folder" -GNinja
+cmake --build build-cmake --config Release --verbose
+cmake --install build-cmake/ --config Release --verbose
+cd ..
+
+popd
+
+# Set KIVY_DEPS_ROOT to the dist folder
+Write-Host "Setting KIVY_DEPS_ROOT to $dist_folder"
+$env:KIVY_DEPS_ROOT = $dist_folder
+
+Write-Host "Dependencies built successfully."
---
+++ win/sdl3.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import, print_function
+from .common import *
+
+__version__ = "0.0.1"
+
+
+def get_sdl3(cache, build_path, arch, package, output, download_only=False):
+    data = []
+    base_dll_dir = join(build_path, "bin")
+
+    sources = {
+            'bin': join(build_path, 'bin'),
+            'lib': join(build_path, 'lib'),
+            'include': join(build_path, 'include')
+            }
+
+    for d, src in sources.items():
+        # bin goes to python/share/kivy_package
+        for dirpath, dirnames, filenames in os.walk(src):
+            root = dirpath
+
+            if d == 'bin':
+                base = join('share', package, 'bin')
+            elif d == 'lib':
+                base = 'libs'
+            elif d == 'include':
+                base = 'include'
+            else:
+                assert False
+
+            dirpath = dirpath.replace(src, '')
+            if dirpath and dirpath[0] == sep:
+                dirpath = dirpath[1:]
+
+            for filename in filenames:
+                is_dev = d != 'bin'
+                if d == 'lib':
+                    if filename.endswith('lib'):
+                        base = 'libs'
+                    else:
+                        base = join('share', package, 'bin')
+                        is_dev = False
+                data.append((
+                    join(root, filename), join(d, dirpath, filename),
+                    join(base, dirpath), is_dev))
+
+    make_package(
+        join(build_path, "project"), package, data, __version__, output, "MIT"
+    )
+
+
+if __name__ == "__main__":
+    parse_args(get_sdl3)
